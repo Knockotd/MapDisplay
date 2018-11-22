2b5a772a1fdb4c89ed09280c2d1ca209a6270812
a503-26 <1>
2018-11-22 12:03:54 +0900

    Initial Commit

---
+++ MapDisplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDisplay
+//
+//  Created by 503-26 on 22/11/2018.
+//  Copyright © 2018 map. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
